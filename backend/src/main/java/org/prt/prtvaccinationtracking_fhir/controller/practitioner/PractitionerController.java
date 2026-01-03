61d2a406c7372002de0931992d67049116d01883
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2026-01-03 16:41:39 +0200

    comments practitioner controller

--- backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/PractitionerController.java
+++ backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/PractitionerController.java
@@ -1,9 +1,13 @@
 package org.prt.prtvaccinationtracking_fhir.controller.practitioner;
 
+import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.prt.prtvaccinationtracking_fhir.dto.practitioner.CreatePractitionerRequestDTO;
 import org.prt.prtvaccinationtracking_fhir.dto.practitioner.PractitionerDTO;
 import org.prt.prtvaccinationtracking_fhir.service.practitioner.PractitionerService;
 import org.springframework.web.bind.annotation.*;
+/**
+ * Register practitioner - endpoint (creates new practitioner)
+ */
 
 @RestController
 @RequestMapping("/api/practitioners")
