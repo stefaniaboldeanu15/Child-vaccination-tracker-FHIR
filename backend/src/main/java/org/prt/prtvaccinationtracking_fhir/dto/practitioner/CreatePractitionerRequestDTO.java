d0497e3c3c28596cb6bc511e9f3a53d3a02db511
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2026-01-03 16:15:15 +0200

    Create Patient Request DTO

---
+++ backend/src/main/java/org/prt/prtvaccinationtracking_fhir/dto/practitioner/CreatePractitionerRequestDTO.java
@@ -0,0 +1,42 @@
+package org.prt.prtvaccinationtracking_fhir.dto.practitioner;
+
+/**
+ * DTO used to create a new Practitioner.
+ */
+public class CreatePractitionerRequestDTO {
+
+    /**
+     * Business identifier (e.g. username, license number)
+     * Will be stored as Practitioner.identifier.value
+     */
+    private String practitionerIdentifier;
+
+    private String firstName;
+    private String lastName;
+
+    // --- getters & setters ---
+
+    public String getPractitionerIdentifier() {
+        return practitionerIdentifier;
+    }
+
+    public void setPractitionerIdentifier(String practitionerIdentifier) {
+        this.practitionerIdentifier = practitionerIdentifier;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
