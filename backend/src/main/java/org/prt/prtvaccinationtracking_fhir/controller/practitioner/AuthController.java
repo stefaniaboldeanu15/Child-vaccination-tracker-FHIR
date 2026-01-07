2c29c3f4307109b05d4c7ccccc6b3c352ad26fe8
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2026-01-07 19:44:06 +0200

    update Auth Controller

--- backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/AuthController.java
+++ backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/AuthController.java
@@ -1,10 +1,12 @@
 package org.prt.prtvaccinationtracking_fhir.controller.practitioner;
 
 import jakarta.annotation.PostConstruct;
+import org.prt.prtvaccinationtracking_fhir.dto.practitioner.CreatePractitionerRequestDTO;
 import org.prt.prtvaccinationtracking_fhir.dto.practitioner.LoginRequestDTO;
 import org.prt.prtvaccinationtracking_fhir.dto.practitioner.LoginResponseDTO;
 //import org.prt.prtvaccinationtracking_fhir.dto.practitioner.RegistrationRequestDTO;
 //import org.prt.prtvaccinationtracking_fhir.dto.practitioner.RegistrationResponseDTO;
+import org.prt.prtvaccinationtracking_fhir.dto.practitioner.PractitionerDTO;
 import org.prt.prtvaccinationtracking_fhir.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ public class AuthController {
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
+
         this.authService = authService;
     }
 
@@ -35,11 +38,10 @@ public class AuthController {
         return "Hello! API is running.";
     }
 
-    // 1. Login endpoint - PRACTITIONER
+    // Login endpoint - PRACTITIONER
     @PostMapping("/auth/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
         LoginResponseDTO response = authService.authenticate(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 }
