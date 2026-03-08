445667242b7a90592de47d70ea17040982ca78c5
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2026-03-08 21:04:54 +0200

    controllers

--- backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/EncounterController.java
+++ backend/src/main/java/org/prt/prtvaccinationtracking_fhir/controller/practitioner/EncounterController.java
@@ -1,12 +1,14 @@
 package org.prt.prtvaccinationtracking_fhir.controller.practitioner;
 
 import jakarta.validation.Valid;
-import org.prt.prtvaccinationtracking_fhir.dto.practitioner.encounter.*;
+import org.prt.prtvaccinationtracking_fhir.dto.practitioner.encounter.CreateEncounterRequestDTO;
+import org.prt.prtvaccinationtracking_fhir.dto.practitioner.encounter.EncounterDTO;
+import org.prt.prtvaccinationtracking_fhir.dto.practitioner.encounter.UpdateEncounterRequestDTO;
 import org.prt.prtvaccinationtracking_fhir.service.practitioner.EncounterService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/practitioner/encounters")
+@RequestMapping("/api/practitioner")
 public class EncounterController {
 
     private final EncounterService service;
@@ -15,18 +17,20 @@ public class EncounterController {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping("/encounters")
     public EncounterDTO create(@RequestBody @Valid CreateEncounterRequestDTO dto) {
         return service.create(dto);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/encounters/{id}")
     public EncounterDTO getById(@PathVariable String id) {
         return service.getById(id);
     }
 
-    @PutMapping("/{id}")
-    public EncounterDTO update(@PathVariable String id, @RequestBody @Valid UpdateEncounterRequestDTO dto) {
+    @PutMapping("/encounters/{id}")
+    public EncounterDTO update(@PathVariable String id,
+                               @RequestBody @Valid UpdateEncounterRequestDTO dto) {
         return service.update(id, dto);
     }
+
 }
